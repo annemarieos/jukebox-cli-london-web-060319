6094d47bf00e41619bf286239c3d5cdf1703e0a4
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-06-01 15:49:28 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -39,7 +39,7 @@ end
 
 def play(songs) 
   puts "please enter song name or number:"
-  song_to_be_played = gets.chomp
+  song_to_play = gets.chomp
     if (1..9).to_a.include?(song_to_play.to_i)
     puts "Playing #{songs[song_to_play.to_i - 1]}"
   elsif songs.include?(song_to_play)
